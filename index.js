96177c6d69170120a8a419e1a4a0e4501333334b
iSekkachi <santiago.banol@upb.edu.co>
2024-04-25 18:36:17 -0500

    Variable Declaration

---
+++ index.js
@@ -0,0 +1,9 @@
+const shoppingCartIcon = document.getElementById("icon-shopping-cart");
+const shoppingCartContainer = document.getElementById(
+    "icon-shopping-cart-container"
+);
+const shoppingCart = document.getElementById("shopping-cart");
+const products = document.getElementById("products-section");
+const iconExit = document.getElementById("icon-exit");
+const selectedProducts = document.getElementsByClassName("product-container");
+const buyButton = document.getElementById("buy-button");
